c522879acad5dfa9c85c7b57041446a441818610
iiqbalh <iqbalhernadi17@gmail.com>
2024-06-24 13:43:52 +0700

    search done

--- server.js
+++ server.js
@@ -15,22 +15,71 @@ app.use(bodyParser.json())
 
 
 app.get('/', (req, res) => {
-    db.all('SELECT * FROM data', (err, rows) => {
+    const { name, height, weight, starDate, endDate, isMarried } = req.query;
+
+    const wheres = [];
+    const params = [];
+    const count = []
+
+    if (name) {
+        wheres.push('name like "%" || ? || "%"')
+        params.push(name)
+    }
+
+    if (height) {
+        wheres.push('height = ?')
+        params.push(height)
+    }
+
+    if (weight) {
+        wheres.push('weight = ?')
+        params.push(weight)
+    }
+
+    if (starDate && endDate) {
+        wheres.push('birthdate BETWEEN ? and ?');
+        params.push(starDate, endDate);
+        count.push(starDate, endDate);
+    } else if (starDate) {
+        wheres.push('birthdate >= ?');
+        params.push(starDate);
+        count.push(starDate);
+    } else if (endDate) {
+        wheres.push('birthdate <= ?');
+        params.push(endDate);
+        count.push(endDate);
+    }
+
+    if (isMarried) {
+        wheres.push('married = ?')
+        params.push(isMarried)
+    }
+
+    let sql = 'SELECT * FROM data';
+
+    if (wheres.length > 0) {
+        sql += ` WHERE ${wheres}`
+    }
+
+    db.all(sql, params, (err, rows) => {
         if (err) return res.send(err)
         res.render('read', { rows })
     })
 })
 
 app.get('/add', (req, res) => {
-    res.render('form')
+    res.render('form', { rows: {} })
 })
 
-app.get('/edit', (req, res) => {
-    db.all('SELECT * FROM data', (err, rows) => {
-        if (err) return res.send(err)
-        res.render('form', { rows })
-    })
-})
+// app.get('/edit/:index', (req, res) => {
+//     const id = req.params.index || 1
+//     console.log(id)
+
+//     db.all('SELECT * FROM data WHERE id = ?' [id], (err, rows) => {
+//         if (err) return res.send(err)
+//         res.render('form', { rows })
+//     })
+// })
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000')
